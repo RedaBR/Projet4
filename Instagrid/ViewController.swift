0431f31e60af9957df48c51684a67cae4e313340
Reda <>
2021-09-13 13:21:07 +0200

    bug fix et accés photos

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -7,175 +7,180 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
-    //var imagePicker:UIImagePickerController!
+    var imagePicker:UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        
         container.isHidden = true
         
     }
     
     var state = 0
-    var image1:UIImage?
-    var image2:UIImage?
+    var clickedImg = -1
 
     @IBOutlet weak var selected: UIImageView!
-    
-    
     @IBOutlet weak var selected2: UIImageView!
-    
-    
-    
     @IBOutlet weak var selected3: UIImageView!
-    
     
     @IBOutlet weak var container: UIView!
     
     
     @IBOutlet weak var grid1: UIView!
-    
-    @IBOutlet weak var img1D1: UIImageView!
-    
-    @IBOutlet weak var img2D1: UIImageView!
-    
-    @IBOutlet weak var img3D1: UIImageView!
-    
-    
-    @IBOutlet weak var grid2: UIView!
-    
-    
-    
-    @IBOutlet weak var img1D2: UIImageView!
-    
-    @IBOutlet weak var img2D2: UIImageView!
-    
-    @IBOutlet weak var img3D2: UIImageView!
+    @IBOutlet weak var img1G1: UIImageView!
+    @IBOutlet weak var img2G1: UIImageView!
+    @IBOutlet weak var img3G1: UIImageView!
     
 
+    
+    @IBOutlet weak var grid2: UIView!
+    @IBOutlet weak var img1G2: UIImageView!
+    @IBOutlet weak var img2G2: UIImageView!
+    @IBOutlet weak var img3G2: UIImageView!
+    
+    
+    
+    
     @IBOutlet weak var grid3: UIView!
     
+    @IBOutlet weak var img1G3: UIImageView!
+    @IBOutlet weak var img2G3: UIImageView!
+    @IBOutlet weak var img3G3: UIImageView!
+    @IBOutlet weak var img4G3: UIImageView!
     
-    @IBOutlet weak var img1D3: UIImageView!
-    
-    @IBOutlet weak var img2D3: UIImageView!
-    
-    @IBOutlet weak var img3D3: UIImageView!
-    
-    @IBOutlet weak var img4D3: UIImageView!
     
 
     @IBAction func buttonSelected() {
-        
-        selected.isHidden = false
-        selected2.isHidden = true
-        selected3.isHidden = true
-        
-        container.isHidden = false
-        grid1.isHidden = false
-        grid2.isHidden = true
-        grid3.isHidden = true
-
-        
+        state = 0
+        displaySelection(selection: state)
     }
-    
     
     @IBAction func buttonSelected2() {
-        selected.isHidden = true
-        selected2.isHidden = false
-        selected3.isHidden = true
-
-        container.isHidden = false
-        
-        grid1.isHidden = true
-        grid2.isHidden = false
-        grid3.isHidden = true
+        state = 1
+        displaySelection(selection: state)
     }
-    
-    
     
     @IBAction func buttonSelected3() {
-        selected3.isHidden = false
-        selected2.isHidden = true
-        selected.isHidden = true
+        state = 2
+        displaySelection(selection: state)
+
+    }
+    
+    func displaySelection(selection:Int) {
+        
+        selected.isHidden = (selection != 0)
+        selected2.isHidden = (selection != 1)
+        selected3.isHidden = (selection != 2)
+
+        grid1.isHidden = (selection != 0)
+        grid2.isHidden = (selection != 1)
+        grid3.isHidden = (selection != 2)
         
         container.isHidden = false
-        grid1.isHidden = true
-        grid2.isHidden = true
-        grid3.isHidden = false
-        
-        //displayDispo3()
-        //takePhoto()
     }
     
     
+    @IBAction func button1G1() {
+        clickedImg = 0
+        takePhoto()
+    }
     
-    @IBAction func button1D1() {
+    @IBAction func button2G1() {
+        clickedImg = 1
+        takePhoto()
+    }
+    
+    @IBAction func button3G1() {
+        clickedImg = 2
+        takePhoto()
     }
     
     
+    @IBAction func button1G2() {
+        clickedImg = 0
+        takePhoto()
+    }
     
-    @IBAction func button2D1() {
+    @IBAction func button2G2() {
+        clickedImg = 1
+        takePhoto()
+    }
+    
+    @IBAction func button3G2() {
+        clickedImg = 2
+        takePhoto()
+    }
+    
+    @IBAction func button1G3() {
+        clickedImg = 0
+        takePhoto()
+    }
+    
+    @IBAction func button2G3() {
+        clickedImg = 1
+        takePhoto()
+    }
+    
+    @IBAction func button3G3() {
+        clickedImg = 2
+        takePhoto()
+    }
+    
+    @IBAction func button4G3() {
+        clickedImg = 3
+        takePhoto()
     }
     
     
-    
-    @IBAction func button3D1() {
-    }
-    
-    
-    
-    
-    @IBAction func button1D2() {
-    }
-    
-    
-    @IBAction func button2D2() {
-    }
-    
-    
-    @IBAction func button3D2() {
-    }
-    
-    
-    @IBAction func button1D3() {
-    }
-    
-    
-    @IBAction func button2D3() {
-    }
-    
-    
-    @IBAction func button3D3() {
-    }
-    
-    @IBAction func button4D3() {
-    }
-    
-    //func takePhoto() {
-        //imagePicker = UIImagePickerController()
-        //imagePicker.delegate = self
-        //imagePicker.sourceType = .camera
-        //present(imagePicker, animated: true, completion: nil)
-        
-    //}
-    
-    //func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        
-    //}
-    
-    //func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-    //}
-    
-    
-  
     
    
     
     
+   
     
+  
+    
+    
+   
+    
+   
+    
+   
+    
+   
+   
+    
+    func takePhoto() {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        clickedImg = -1
+    }
+    
+
+    
+    func addImage(img:UIImage) {
+        switch (state) {
+            case 0:
+                if (clickedImg == 0) {
+                    img1G1.image = img
+                } else if (clickedImg == 1) {
+                    img2G1.image = img
+                }
+                break
+            case 1: break
+            case 2: break
+            default: break
+        }
+    }
+ 
 }
 
